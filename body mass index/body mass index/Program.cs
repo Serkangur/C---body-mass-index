ce55eb858aca67b66d121a0b097589c417959887
Serkan <94819174+Serkangur@users.noreply.github.com>
2021-11-22 16:05:12 +0300

    C# body mass index
    C# body mass index

---
+++ body mass index/body mass index/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace body_mass_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kilo değerinizi giriniz");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("boy değerinizi girin");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+            double bke = kilo / (boy * boy);
+
+            if (bke < 18)
+            {
+                Console.WriteLine("zayıfsın ha");
+                Console.ReadLine();
+            }
+
+            else if (bke > 18 && bke < 25)
+            {
+                Console.WriteLine("normalsın");
+                Console.ReadLine();
+            }
+
+            else if (bke > 25)
+            {
+                Console.WriteLine("obezsin pis obez seni");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
